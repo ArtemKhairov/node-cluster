9359aecd5edccfc82ce474f5b91ea09299958fd4
unknown <artemxairoff@yandex.ru>
2020-09-22 22:33:18 +0700

    modified cluster

--- worker.js
+++ worker.js
@@ -8,3 +8,24 @@ http.createServer((req, res) => {
   .listen(8800, () => {
     console.log(`Server started. Pid: ${pid}`);
   }); 
+
+process.on("SIGINT", () => {
+  console.log('Signal is SIGINT');
+  server.close(() => {
+    process.exit(0);
+  })
+});
+
+process.on("SIGTERM", () => {
+  console.log('Signal is SIGTERM');
+  server.close(() => {
+    process.exit(0);
+  })
+});
+
+process.on("SIGUSR2", () => {
+  console.log('Signal is SIGUSR2');
+  server.close(() => {
+    process.exit(1);
+  })
+});
